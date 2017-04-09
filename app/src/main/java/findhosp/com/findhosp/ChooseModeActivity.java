9d1600e942af14e5743761ae9aa7e867b54d4a38
bahbah <peerapat_men@hotmail.com>
2017-04-09 13:12:17 +0700

    ExtraIndex Ok

--- app/src/main/java/findhosp/com/findhosp/ChooseModeActivity.java
+++ app/src/main/java/findhosp/com/findhosp/ChooseModeActivity.java
@@ -2,12 +2,49 @@ package findhosp.com.findhosp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
-public class ChooseModeActivity extends AppCompatActivity {
+public class ChooseModeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView nearImageView, filterImageView;
+    private int indexAnInt;
+    private String tag = "9AprilV1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_mode);
+
+        //Initial View
+        initialView();
+
+        //Get Value From Intent
+        getValueFromIntent();
+
+        //Image Controller
+        imageController();
+
+    }   //Main Method
+
+    private void getValueFromIntent() {
+        indexAnInt = getIntent().getIntExtra("Index", 0);
+        Log.d(tag, "Index==>" + indexAnInt);
     }
-}
+
+    private void imageController() {
+        nearImageView.setOnClickListener(ChooseModeActivity.this);
+        filterImageView.setOnClickListener(ChooseModeActivity.this);
+    }
+
+    private void initialView() {
+        nearImageView = (ImageView) findViewById(R.id.imvNear);
+        filterImageView = (ImageView) findViewById(R.id.imvFilter);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}   //Main Class
--- app/src/main/java/findhosp/com/findhosp/MainActivity.java
+++ app/src/main/java/findhosp/com/findhosp/MainActivity.java
@@ -1,13 +1,55 @@
 package findhosp.com.findhosp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //Explicit
+    private ImageView humanImageView, animalImageView;
+    private int anInt = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Initial View
+        initialView();
+
+        //Image Controller
+        imageController();
+
+    }   //Main Method
+
+    private void imageController() {
+        humanImageView.setOnClickListener(MainActivity.this);
+        animalImageView.setOnClickListener(MainActivity.this);
     }
-}
+
+    private void initialView() {
+        humanImageView = (ImageView) findViewById(R.id.imvHuman);
+        animalImageView = (ImageView) findViewById(R.id.imvAnimal);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view==humanImageView) {
+            anInt = 0;
+        }
+
+        if (view==animalImageView) {
+            anInt = 1;
+        }
+
+        Intent intent = new Intent(MainActivity.this, ChooseModeActivity.class);
+        intent.putExtra("Index", anInt);
+        startActivity(intent);
+
+    }//onClick
+}   //Main Class
