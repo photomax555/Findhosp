128ab3569a4c3203ec0d1975c35d5780464a4373
winter <bestlovedream23@gmail.com>
2017-04-09 11:27:02 +0700

    Create ChosseMode ok

---
+++ app/src/main/java/findhosp/com/findhosp/ChooseModeActivity.java
@@ -0,0 +1,13 @@
+package findhosp.com.findhosp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+    }
+}
