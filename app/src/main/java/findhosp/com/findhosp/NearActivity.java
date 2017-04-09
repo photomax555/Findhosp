ba5c98555253697df9ea1fc0923f99cdead48895
bahbah <peerapat_men@hotmail.com>
2017-04-09 14:04:56 +0700

    GetIntent Ok

--- app/src/main/java/findhosp/com/findhosp/NearActivity.java
+++ app/src/main/java/findhosp/com/findhosp/NearActivity.java
@@ -2,6 +2,7 @@ package findhosp.com.findhosp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,27 +14,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class NearActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private int indexAnInt;
+    private String tag = "9Aprilv2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_near);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        //Get Value From Intent
+        getValueFromIntent();
+
+        // Create Map Fragment
+        createFragment();
+
+    }   //Main Method
+
+    private void getValueFromIntent() {
+        indexAnInt = getIntent().getIntExtra("Index", 0);
+        Log.d(tag, "Index==>" + indexAnInt);
+    }
+
+    private void createFragment() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +49,5 @@ public class NearActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+    }   //onMap
+}   //Main Class
